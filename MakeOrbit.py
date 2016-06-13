c41b82ea2b36215b29651596890bd147ade60ede
nlifset1 <nlifset1@gmail.com>
2016-06-13 13:38:37 -0400

    fixed

--- MakeOrbit.py
+++ MakeOrbit.py
@@ -1,5 +1,8 @@
+import Orbit_Code
+reload(Orbit_Code)
+
 from Orbit_Calculator import Orbit_Calculator
 
-orbit = Orbit_Calculator(4,0.1,8.,0.3,7.8,0.,3.,223.)
+orbit = Orbit_Code.Orbit_Calculator(4,0.1,8.,0.3,7.8,0.,3.,223.)
 
 orbit.makeOrbit()
