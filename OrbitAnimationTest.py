7f6c6bb9b6aceeb5c7198be135716f7a9430cc95
lbarban1 <LBarbano@dhcp-130-58-97-118.swarthmore.edu>
2016-06-15 16:10:11 -0400

    Put the file loader in the animation

--- OrbitAnimationTest.py
+++ OrbitAnimationTest.py
@@ -3,9 +3,26 @@ from matplotlib import animation
 import Orbit_Code 
 reload(Orbit_Code)
 
-orbit = Orbit_Code.Orbit_Calculator(4,5.0,8,.4,7.8,0.,3.,230.) 
-f = np.load("/Users/LBarbano/Desktop/QP_Dump/qp_(m=4)_(t=5.0)_(CR=8.0)_(eps=0.4)_(x0=7.8)_(y0=0.0)_(vx0=3.0)_(vy0=230.0).npy")
+def parseFilename(filename):
+   
+    parts = filename.split("=")
+    args = []
+    for l in range(1, len(parts)):
+        num = float(parts[l].split(")")[0])
+        args.append(num)
+    return args
+    
+filepath = "/Users/LBarbano/Desktop/QP_Dump/"
+
+#Copy and paste the filename of the desired qp file here
+filename = "qp_(m=4)_(t=5.0)_(CR=8.0)_(eps=0.4)_(x0=7.8)_(y0=0.0)_(vx0=3.0)_(vy0=230.0).npy"
+
+filename = filepath + filename 
+a = parseFilename(filename)
+orbit = Orbit_Code.Orbit_Calculator(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7])
+f = np.load(filename)
 orbit.setqp(f)
+
 fig,ax = orbit.plot(2)
 
 RetardingConstant = 0.001*orbit.getNSteps() #Animation related stuff
