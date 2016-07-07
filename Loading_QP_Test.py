5b8c23037cf80caf9a29457103b5a9d24794b2e1
lbarban1 <LBarbano@dhcp-130-58-97-118.swarthmore.edu>
2016-07-07 10:37:38 -0400

    Finished table making files

--- Loading_QP_Test.py
+++ Loading_QP_Test.py
@@ -19,25 +19,32 @@ def parseList(files):
     for l in range(0,len(files)):
         table[l] = parseFilename(files[l])
     return table
-
+ 
 def genTable(filepath):
     table = []
     for dirpath, dirnames, files in os.walk(filepath):
         for f in files:
-            if f != ".DS_Store":
-                if (f.endswith("tar.gz")):
-                    tar = tarfile.open(f, "r:gz")
-                    tar.extractall()
-                    tar.close()
+            fullpath = os.path.join(dirpath, f) #get full path of subject file
+            if f != ".DS_Store" and not f.endswith("tar.gz"):
                 a = parseFilename(f) #Turn filename into numpy array of initial conditions
                 orbit = Orbit_Code.Orbit_Calculator(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8])
                 fullpath = os.path.join(dirpath, f) #get full path of subject file
-                data = np.load(fullpath)    #need to change around order of data columns for real thing
-                orbit.setqp(data)
+                data = np.loadtxt(fullpath) #need to change around order of data columns for real thing
+                data = data.astype(float)   #change to float
+                t = data[:,0]   #next two lines switch order of t,x,y,vx,vy to x,y,vx,vy,t
+                data = np.c_[data[:,1:5] ,t] 
+                orbit.setqp(data)   
                 lamsp = orbit.Lam_special()
                 Lz = orbit.findLz()
                 table.append([dirpath+f,a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],lamsp,Lz[0],Lz[1],Lz[2],Lz[3],Lz[4]]) 
-                
     return np.array(table)
 
-
+def unTar(filepath):
+    for dirpath, dirnames, files in os.walk(filepath):
+        for f in files:
+            fullpath = os.path.join(dirpath, f) #get full path of subject file
+            if f.endswith("tar.gz"):
+                tar = tarfile.open(fullpath, "r:gz")
+                tar.extractall(path = filepath)
+                tar.close()
+    return
--- MakeTable.py
+++ MakeTable.py
@@ -1,5 +1,17 @@
 import Loading_QP_Test as tableMaker
 reload(tableMaker)
+import numpy as np
+from timeit import default_timer
 
-filepath = "/Users/LBarbano/Desktop/QP_Dump/"
-table = tableMaker.genTable(filepath)
+start = default_timer()
+print("Calculating...")
+
+filepath = "/Users/LBarbano/Github/Summer-2016/Trapped_Orbital_Integrator-master/" #filepath to tar files
+#tableMaker.unTar(filepath) #untar the files
+table = tableMaker.genTable(filepath) #make table of analysis stuff
+
+duration = default_timer() - start
+print "time: %s s" % str(duration) 
+
+dataFilePath = "/Users/LBarbano/Github/Summer-2016/table.txt"
+np.savetxt(dataFilePath, table.astype(str), delimiter = " ",fmt='%s')   
